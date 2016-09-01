910c8d0834dc2566ea3fe6f7b90ac9f2aa7e51c4
Mr-Squirrel <hugo.leandersson@telia.com>
2016-09-01 16:08:35 +0200

    simplifying class exports

--- classes/LessWatch.class.js
+++ classes/LessWatch.class.js
@@ -1,48 +1,45 @@
 'use strict';
+var s = g.settings,
+    once = true;
 
 // gulp tasks for LESS compilation
-module.exports = function() {
-  var s = g.settings,
-      once = true;
+module.exports = class LessWatch {
+  constructor() {
+    this.settings = s.LessWatch;
+    this.paths = this.settings.paths;
 
-  return class LessWatch {
-    constructor() {
-      this.settings = s.LessWatch;
-      this.paths = this.settings.paths;
+    once && this.setup();
+    once = false;
+  }
 
-      once && this.setup();
-      once = false;
-    }
+  setup() {
+    // create tasks
+    this.tasks();
+    m.gulp.start('less-watch');
+  }
 
-    setup() {
-      // create tasks
-      this.tasks();
-      m.gulp.start('less-watch');
-    }
+  tasks() {
+    var me = this;
+    m.gulp.task('build-css-from-less', [], function() {
+      try {
+        return m.gulp
+          .src(me.paths.lessInput)
+          .pipe(m.gulpless())
+          .pipe(m.gulpcleancss())
+          .pipe(m.gulp.dest(me.paths.cssOutput));
+      } catch(e) {
+        console.log("LESS compilation err\n\n", e.stack); return null;
+      }
+    });
 
-    tasks() {
-      var me = this;
-      m.gulp.task('build-css-from-less', [], function() {
-        try {
-          return m.gulp
-            .src(me.paths.lessInput)
-            .pipe(m.gulpless())
-            .pipe(m.gulpcleancss())
-            .pipe(m.gulp.dest(me.paths.cssOutput));
-        } catch(e) {
-          console.log("LESS compilation err\n\n", e.stack); return null;
-        }
-      });
+    m.gulp.task('less-watch', ['build-css-from-less'], function() {
+      m.gulp
+        .watch(me.paths.watchDirs, ['build-css-from-less'])
+        .on('change', me.reportChange);
+    });
+  }
 
-      m.gulp.task('less-watch', ['build-css-from-less'], function() {
-        m.gulp
-          .watch(me.paths.watchDirs, ['build-css-from-less'])
-          .on('change', me.reportChange);
-      });
-    }
-
-    reportChange() {
-      console.log("LESS files changed, new CSS generated...");
-    }
-  };
+  reportChange() {
+    console.log("LESS files changed, new CSS generated...");
+  }
 };
