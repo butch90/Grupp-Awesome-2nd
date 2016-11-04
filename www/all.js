42ed86fcbbbfe4b42560349eb3be3aa7d180766c
Olle Bengtsson <Olle Bengtsson>
2016-11-04 11:57:13 +0100

    .

--- www/all.js
+++
@@ -1,9 +0,0 @@
-document.write(
-  [
-    // libs
-    '/libs/jquery.min.js',
-    '/libs/bootstrap.min.js'
-  ].map(function(scriptSrc){
-    return '<script src="' + scriptSrc + '"></script>';
-  }).join('')
-);
