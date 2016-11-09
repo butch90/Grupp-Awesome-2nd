166aca005c7f1e83f6eefb970780b1b00a946f67
Marcus Wendel <marcuswendel558@hotmail.com>
2016-11-09 13:55:19 +0100

    update

--- classes/server.class.js
+++ classes/server.class.js
@@ -23,14 +23,10 @@ module.exports = class Server {
 		
 		this.app.listen(g.port, function(){
 
-<<<<<<< HEAD
-		  	console.log("runing at port: ", g.settings.port);
-=======
 			var date = new Date();
 			var time = ((date.getHours() < 10) ? ('0' + date.getHours()) : date.getHours()) + ':' + ((date.getMinutes() < 10) ? ('0' + date.getMinutes()) : date.getMinutes());
 
 		  	console.log("Server started at port: ", g.port + '\nTime: ' + time);
->>>>>>> 02c0699758de5f1e63330f7dd37096e490f680e6
 		});
 	}
 }
