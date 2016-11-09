7c35bf0144d7acc1d2b72c9ad1186d34a4ad93d2
Marcus Wendel <marcuswendel558@hotmail.com>
2016-11-09 13:58:07 +0100

    merge fix

--- classes/Routing.class.js
+++ classes/Routing.class.js
@@ -17,26 +17,14 @@ module.exports = class Routing {
 		me[req.method](model, req.params, req.body, req, res);
     }
 
-<<<<<<< HEAD
+
 		DELETE(model, params, body, req, res) {
-	    if(!params.modelID) { this.error({error: 'Missing ID!'}, res); return; }
+		  if (!params.modelID) { this.error({error: 'Missing ID!'}, res); return; }
 
-	    var me = this;
-	   	model.findByIdAndRemove(params.modelID, function(err, result) {
-	    	if(err) { me.error(err, res); return; }
-	      res.json(true);
-	    });
-	  }
-	}
-=======
-	DELETE(model, params, body, req, res) {
-	    if (!params.modelID) { this.error({error: 'Missing ID!'}, res); return; }
-
-		var me = this;
-		model.findByIdAndRemove(params.modelID, function(err, result) {
-			if (err) { me.error(err, res); return; }
+			var me = this;
+			model.findByIdAndRemove(params.modelID, function(err, result) {
+				if (err) { me.error(err, res); return; }
 			res.json(true); // respond with result
 		});
 	 }
->>>>>>> 02c0699758de5f1e63330f7dd37096e490f680e6
 }
