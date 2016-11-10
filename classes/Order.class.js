aa1ae2a817dfae8e04b2d86f471b69fd6403f53c
Olle Bengtsson <Olle Bengtsson>
2016-11-10 15:04:00 +0100

    cleaned up in order class

--- classes/Order.class.js
+++ classes/Order.class.js
@@ -14,16 +14,14 @@ module.exports = class Order {
 				res.sendStatus(404);
 				return;
 			}
-			var data = req.body || {},
-				params = req.params || {};
+
 			me[req.method](req, res);
 		});
 	}
 
-	POST(req, res, data) {
+	POST(req, res) {
 
-
-		this.order.create(data, function(err, data) {
+		this.order.create(req.body, function(err, data) {
 			if(err) {
 				console.log(err.stack);
 				res.json(err.stack);
@@ -37,29 +35,28 @@ module.exports = class Order {
 
 		var query = req.params.id ? 'findById' : 'find';
 		var data = req.params.id ? req.params.id : {};
-		this.order[query](data, function(err, data) {
+
+		this.order[query](data, function(err, result) {
 			if(err) {
 				res.json(err.stack);
 			}
-			res.json(data);
+			res.json(result);
 		})
 	}
 	PUT(req, res) {
-		var data = req.body;
-		var query = req.params.id;
-		this.order.findByIdAndUpdate(query, data, function(err, result) {
+
+		this.order.findByIdAndUpdate(req.params.id, req.body, function(err, data) {
 			if(err) {
 				res.json(err.stack);
 				console.log(err.stack);
 			}
-			res.json(data);
+			res.json(req.body);
 		});
 
 	}
 	DELETE(req, res) {
-		var data = req.body;
-		var query = req.params.id;
-		this.order.findByIdAndRemove(query, data, function(err, result) {
+
+		this.order.findByIdAndRemove(req.params.id, function(err, data) {
 			if(err) {
 				res.json(err.stack);
 				console.log(err.stack);
