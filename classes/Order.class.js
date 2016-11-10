b4d61bf04657cd508ccf8ebbe83908fd21a3467f
Olle Bengtsson <Olle Bengtsson>
2016-11-10 14:22:38 +0100

    full crud for order class complete

--- classes/Order.class.js
+++ classes/Order.class.js
@@ -3,7 +3,7 @@ module.exports = class Order {
 	constructor(express) {
 		this.app = express;
 		this.dataBase  = new g.classes.Mongo();
-		this.model = this.dataBase.getModel('Order');
+		this.order = this.dataBase.getModel('Order');
 
 		this.router();
 	}
@@ -14,15 +14,16 @@ module.exports = class Order {
 				res.sendStatus(404);
 				return;
 			}
+			var data = req.body || {},
+				params = req.params || {};
 			me[req.method](req, res);
 		});
 	}
 
-	POST(req, res) {
+	POST(req, res, data) {
 
-		var data = req.body || {};
 
-		this.model.create(data, function(err, data) {
+		this.order.create(data, function(err, data) {
 			if(err) {
 				console.log(err.stack);
 				res.json(err.stack);
@@ -34,14 +35,38 @@ module.exports = class Order {
 	}
 	GET(req, res) {
 
-		var method = req.params.id ? 'findById' : 'find';
+		var query = req.params.id ? 'findById' : 'find';
 		var data = req.params.id ? req.params.id : {};
-		this.model[method](data, function(err, data) {
+		this.order[query](data, function(err, data) {
 			if(err) {
 				res.json(err.stack);
 			}
 			res.json(data);
 		})
+	}
+	PUT(req, res) {
+		var data = req.body;
+		var query = req.params.id;
+		this.order.findByIdAndUpdate(query, data, function(err, result) {
+			if(err) {
+				res.json(err.stack);
+				console.log(err.stack);
+			}
+			res.json(data);
+		});
+
+	}
+	DELETE(req, res) {
+		var data = req.body;
+		var query = req.params.id;
+		this.order.findByIdAndRemove(query, data, function(err, result) {
+			if(err) {
+				res.json(err.stack);
+				console.log(err.stack);
+			}
+			res.json('Removed');
+		});
+
 	}
 
 }
