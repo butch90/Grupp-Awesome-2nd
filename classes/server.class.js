bfa962da96bd994a14edc105a8ae82d2e835e21e
Your Name <you@example.com>
2016-11-11 14:14:30 +0100

    login class

--- classes/server.class.js
+++ classes/server.class.js
@@ -18,21 +18,26 @@ module.exports = class Server {
 	setup(){
 		var me = this;
 
-		/*this.app.get('/', function(req, res){
+		this.app.get('/', function(req, res){
 
 			res.sendFile(g.appRoot + g.webRoot + '/index.html');
-		});*/
+		});
 
 		this.app.use(m.bodyparser.json());
 		this.app.use(m.compression());
 		this.app.use(m.cookieparser());
 		this.app.use(m.bodyparser.urlencoded({extended: false}));
-
+		this.app.use(m.expresssession({
+	      secret: 'bilverkstad',
+	      resave: false,
+	      saveUninitialized: true
+	    }));
 		// Mongoose classes
 		new g.classes.OrderRow(this.app);
 		new g.classes.Order(this.app);
 		new g.classes.Employee(this.app);
         new g.classes.Part(this.app);
+        new g.classes.Login(this.app);
 
 		this.app.listen(me.settings.port, function(){
 
